30c366f3e61ecd2dd8476697283222227d7a8a20
zhanghongbean <zhanghong_bean@126.com>
2019-09-16 16:58:31 +0800

    修改配置信息
    开启自动写入时间戳字段

--- config/app.php
+++ config/app.php
@@ -15,13 +15,13 @@
 
 return [
     // 应用名称
-    'app_name'               => '',
+    'app_name'               => env('app.name', 'ThinkBBS'),
     // 应用地址
-    'app_host'               => '',
+    'app_host'               => env('app.host', 'http://bbs.test'),
     // 应用调试模式
-    'app_debug'              => false,
+    'app_debug'              => env('app.debug', false),
     // 应用Trace
-    'app_trace'              => false,
+    'app_trace'              => env('app.trace', false),
     // 是否支持多模块
     'app_multi_module'       => true,
     // 入口自动绑定模块
