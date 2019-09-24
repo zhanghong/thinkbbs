f333f4bdeadad848c45a2daca090e3f42b6ac1b4
zhanghongbean <zhanghong_bean@126.com>
2019-09-24 17:10:16 +0800

    注册视图表单验证

--- application/index/controller/Register.php
+++ application/index/controller/Register.php
@@ -22,4 +22,24 @@ class Register extends Base
         // 注册成功后跳转到注册表单页面
         $this->success($message, url('create'));
     }
+
+    /**
+     * 验证字段值是否唯一
+     * @Author   zhanghong(Laifuzi)
+     * @DateTime 2019-09-24
+     */
+    public function check_unique(Request $request)
+    {
+        if(!$request->isAjax()){
+            $this->redirect('[page.signup]');
+        }
+
+        $param = $request->post();
+        $is_valid = User::checkFieldUnique($param);
+        if($is_valid){
+            echo("true");
+        }else{
+            echo("false");
+        }
+    }
 }
--- route/route.php
+++ route/route.php
@@ -1,3 +1,7 @@
 <?php
 // 首页
 Route::get('/', 'index/index')->name('page.root');
+// 注册
+Route::post('signup/check_unique', 'register/check_unique')->name('signup.check_unique');
+Route::get('signup', 'register/create')->name('page.signup');
+Route::post('signup', 'register/save')->name('page.signup.save');
