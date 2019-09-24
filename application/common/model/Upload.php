043116417a6cea4b1417d65297e89b819170102e
zhanghongbean <zhanghong_bean@126.com>
2019-09-24 21:57:35 +0800

    上传图片

---
+++ application/common/model/Upload.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\common\model;
+
+class Upload
+{
+   /**
+    * 保存上传图片
+    * @Author   zhanghong(Laifuzi)
+    * @DateTime 2019-02-21
+    * @param    File               $file         文件信息
+    * @return   array
+    */
+    static public function saveImage($file){
+        // 所有上传文件都保存在项目 public/upload 目录里
+        $local_dir = 'uploads';
+        $ds = DIRECTORY_SEPARATOR;
+        $info = $file->rule('md5')->move($local_dir);
+        $save_name = $info->getSaveName();
+        $save_path = $ds.$local_dir.$ds.$save_name;
+
+        return [
+            'ext' => $info->getExtension(),
+            'save_path' => $save_path,
+            'sha1' => $info->hash("sha1"),
+            'md5' => $info->hash("md5"),
+            'size' => $info->getSize(),
+            'origin_name' => $file->getInfo('name'),
+        ];
+    }
+}
---
+++ application/index/controller/Upload.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\index\controller;
+
+use think\Request;
+use app\common\model\Upload as UploadModel;
+
+class Upload extends Base
+{
+    public function create(Request $request)
+    {
+        return $this->save_image($request);
+    }
+
+    public function save(Request $request)
+    {
+        return $this->save_image($request);
+    }
+
+    private function save_image($request)
+    {
+        // 绑定控制名称
+        $backcall = $request->param('backcall');
+        // 图片预览宽度(px)
+        $width = $request->param('width', 100);
+        // 图片预览高度(px)
+        $height = $request->param('height', 100);
+
+        if($request->isPost()){
+            // 保存上传图片
+            $file = $request->file('image');
+            $upload_info = UploadModel::saveImage($file);
+            // 保存成功的图片路径
+            $image = $upload_info['save_path'];
+        }else{
+            // 当前图片路径
+            $image = $request->param('image');
+        }
+
+        $this->assign('backcall', $backcall);
+        $this->assign('width', $width);
+        $this->assign('height', $height);
+        $this->assign('image', $image);
+        return $this->fetch('create');
+    }
+}
