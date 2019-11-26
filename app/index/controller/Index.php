5a9897596d97386ab4fcdc0c4fc18f6236eda36b
zhanghongbean <zhanghong_bean@126.com>
2019-11-26 14:28:00 +0800

    前端布局模板

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -3,10 +3,21 @@ declare (strict_types = 1);
 
 namespace app\index\controller;
 
+use tpadmin\model\Config as ConfigModel;
+
 class Index
 {
     public function index()
     {
-        return '您好！这是一个[index]示例应用';
+        $config = ConfigModel::where('name', ConfigModel::NAME_SITE_SETTING)->find();
+        if (empty($config) || empty($config->value)) {
+            $site = [];
+        } else {
+            $site = json_decode($config->value, true);
+        }
+
+        return view('index', [
+            'site'  => $site,
+        ]);
     }
 }
