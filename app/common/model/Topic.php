d0c42ba04b3316018480e1cfec68e7430fbb165e
zhanghongbean <zhanghong_bean@126.com>
2019-11-26 18:17:13 +0800

    修复 N+1 问题

--- app/common/model/Topic.php
+++ app/common/model/Topic.php
@@ -4,6 +4,7 @@ declare (strict_types = 1);
 namespace app\common\model;
 
 use think\Model;
+use think\Paginator;
 
 /**
  * @mixin think\Model
@@ -20,5 +21,17 @@ class Topic extends Model
     public function category()
     {
         return $this->belongsTo('Category');
+    }
+
+    /**
+     * 分页查询方法
+     * @Author   zhanghong(Laifuzi)
+     * @param    array              $params    请求参数
+     * @param    int                $page_rows 每页显示数量
+     * @return   Paginator
+     */
+    public static function minePaginate(array $param = [], int $per_page = 20): Paginator
+    {
+        return static::with(['user', 'category'])->paginate($per_page);
     }
 }
