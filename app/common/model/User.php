8129b7139bd57ef946c453db22881f59d683b096
zhanghongbean <zhanghong_bean@126.com>
2019-11-26 15:09:15 +0800

    注册视图表单验证

--- app/common/model/User.php
+++ app/common/model/User.php
@@ -10,5 +10,38 @@ use think\Model;
  */
 class User extends Model
 {
-    //
+    /**
+     * 验证字段值是否唯一
+     * @Author   zhanghong(Laifuzi)
+     * @param    array              $data 验证字段和字段值
+     * @param    int                $id   用户ID
+     * @return   bool
+     */
+    public static function checkFieldUnique(array $data, int $id = 0): bool
+    {
+        $field_name = null;
+        // 验证字段名必须存在
+        if (!isset($data['field'])) {
+            return false;
+        }
+        // 验证字段名
+        $field_name = $data['field'];
+
+        // 验证字段值必须存在
+        if (!isset($data[$field_name])) {
+            return false;
+        }
+        $field_value = $data[$field_name];
+
+        $query = static::where($field_name, $field_value);
+        if ($id > 0) {
+            $query->where('id', '<>', $id);
+        }
+
+        if ($query->count()) {
+            return false;
+        }
+
+        return true;
+    }
 }
--- app/index/controller/Register.php
+++ app/index/controller/Register.php
@@ -14,9 +14,37 @@ class Register extends Base
 
     public function save(User $user)
     {
-        // 保存表单提交数据
-        $user->save($this->request->post());
+        if (!$this->request->isPost() || !$this->request->isAjax()) {
+            return $this->error('对不起，你访问页面不存在。');
+        }
+
+        try {
+            // 保存表单提交数据
+            $user->save($this->request->post());
+        } catch (\Exception $e) {
+            return $this->error('对不起，注册失败。');
+        }
+
         // 注册成功后跳转到首页
-        return $this->redirect('/');
+        return $this->success('恭喜你注册成功。', '/');
+    }
+
+    /**
+     * 验证字段值是否唯一
+     * @Author   zhanghong(Laifuzi)
+     */
+    public function check_unique()
+    {
+        if (!$this->request->isPost() || !$this->request->isAjax()) {
+            $this->redirect('[page.signup]');
+        }
+
+        $param = $this->request->post();
+        $is_valid = User::checkFieldUnique($param);
+        if ($is_valid) {
+            echo 'true';
+        } else {
+            echo 'false';
+        }
     }
 }
