af259ca2f73a3a9860c5fcdff0737d5a9c213581
zhanghongbean <zhanghong_bean@126.com>
2019-12-02 21:23:37 +0800

    生成目录结构

--- build.php
+++ build.php
@@ -1,26 +1,30 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
 
 return [
-    // 生成应用公共文件
-    '__file__' => ['common.php'],
+    // 定义共公模块的自动生成
+    'common'     => [
+        '__file__'   => [],
+        '__dir__'    => ['exception', 'model', 'observer', 'validate'],
+        'controller' => [],
+        'model'      => [],
+        'view'       => [],
+    ],
 
-    // 定义demo模块的自动生成 （按照实际定义的文件名生成）
-    'demo'     => [
-        '__file__'   => ['common.php'],
-        '__dir__'    => ['behavior', 'controller', 'model', 'view'],
-        'controller' => ['Index', 'Test', 'UserType'],
-        'model'      => ['User', 'UserType'],
+    // 定义前台模块的自动生成
+    'index'     => [
+        '__file__'   => [],
+        '__dir__'    => ['config', 'controller'],
+        'controller' => ['Index'],
+        'model'      => [],
         'view'       => ['index/index'],
     ],
 
-    // 其他更多的模块定义
+    // 定义后台模块的自动生成
+    'admin'     => [
+        '__file__'   => [],
+        '__dir__'    => ['config', 'controller'],
+        'controller' => ['Index'],
+        'model'      => [],
+        'view'       => [],
+    ],
 ];
