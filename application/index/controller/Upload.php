5246d8a3a7751bf0ae785b8beeac52ef94e57ff7
zhanghongbean <zhanghong_bean@126.com>
2019-12-04 14:30:22 +0800

    用户可以上传话题图片

--- application/index/controller/Upload.php
+++ application/index/controller/Upload.php
@@ -34,7 +34,8 @@ class Upload extends Base
         if($request->isPost()){
             $file = $request->file('image');
             try {
-                $upload_info = UploadModel::saveImage($file, 416);
+                // 把用户头像等比缩小裁剪成宽度 416px
+                $upload_info = UploadModel::saveImage($file, UploadModel::TYPE_AVATAR, UploadModel::MAX_WIDTH_AVATAR);
                 // 保存成功的图片路径
                 $image = $upload_info['save_path'];
             } catch (ValidateException $e) {
@@ -51,5 +52,28 @@ class Upload extends Base
             'image' => $image,
             'error_msg' => $error_msg,
         ]);
+    }
+
+    public function simditor(Request $request)
+    {
+        $file = $request->file('upfile');
+        try {
+            // 正文图片宽度压缩到 1024px
+            $upload_info = UploadModel::saveImage($file, UploadModel::TYPE_CONTENT, UploadModel::MAX_WIDTH_CONTENT);
+            $data = [
+                'status' => true,
+                'msg' => '上传成功',
+                'file_path' => $upload_info['save_path'],
+            ];
+        } catch (\Expection $e) {
+            $data = [
+                'status' => false,
+                'msg' => $e->getMessage(),
+            ];
+        }
+
+        // 变量$data值已经是Simditor编辑器需要返回的格式了
+        // 所以直接调用json()方法返回该数据
+        return json($data);
     }
 }
